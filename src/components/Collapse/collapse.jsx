50eeafc67dd64fa418e0e0e53c6766772574d855
EmmaBroudic <emma.broudic35@gmail.com>
2023-05-29 15:31:17 +0200

    avancées sur la page à propos

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,41 +1,44 @@
 import React, { useState } from 'react';
+import { infos } from '../../data/infos/infos.jsx';
 import flechhaut from '../../images/fleche-haut.png';
 import flechbas from '../../images/fleche-bas.png';
-import Text from '../Text-collapse/text-collapse';
 import './collapse.css';
 
 function Collapse() {
-  // déclarer variable et fonction
-  const [buttonVisible, setButtonVisible] = useState(true);
+  
+  const [infosState, setInfosState] = useState(infos); // Utilisez l'état initial depuis "infos.jsx"
 
-  // fonction pour changer visibilité button
-  const changeButton = () => {
-    setButtonVisible(!buttonVisible);
+  const toggleVisibility = (id) => {
+    setInfosState((prevState) =>
+      prevState.map((info) => {
+        if (info.id === id) {
+          return { ...info, visible: !info.visible }; // Inverse la visibilité de l'élément cliqué
+        }
+        return info;
+      })
+    );
   };
 
   return (
     <div className="collapse-container">
-        <div className="button-container">
-          <p className="titre-collapse">Fiabilité</p>
-          {buttonVisible ? (
-            <button className="buttonup" onClick={changeButton}>
+      {infosState.map((info) => (
+        <div className="button-container" key={info.id}>
+          <h2 className="titre-collapse">{info.title}</h2>
+          {info.visible ? (
+            <button className="buttonup" onClick={() => toggleVisibility(info.id)}>
               <img src={flechhaut} alt="flèche vers le haut" />
             </button>
           ) : (
-            <button className="buttondown" onClick={changeButton}>
+            <button className="buttondown" onClick={() => toggleVisibility(info.id)}>
               <img src={flechbas} alt="flèche vers le bas" />
             </button>
           )}
 
-          {buttonVisible && (
-          <Text>
-            <p>
-              Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
-            </p>
-          </Text>
+          {info.visible && (
+            <p>{info.text}</p>
           )}
         </div>
-
+      ))}
     </div>
   );
 }
