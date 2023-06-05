76b43b12afd040ea7a52b86b8f144c5bceaf5f8e
EmmaBroudic <emma.broudic35@gmail.com>
2023-06-05 10:14:17 +0200

    avancées sur le carroussel

--- src/components/Carroussel/carroussel.jsx
+++ src/components/Carroussel/carroussel.jsx
@@ -1,18 +1,53 @@
+import React, { useState } from 'react';
 import { annonces } from '../../data/annonces/annonces.jsx';
 import { useParams } from 'react-router-dom';
 import './carroussel.css';
 
 function Carroussel() {
-    const { id } = useParams(); // Récupère l'identifiant de l'appartement depuis l'URL
+    const { id } = useParams();
   
     const logement = annonces.find(appartement => appartement.id === id);
 
-    // Utilisez l'identifiant pour récupérer les détails de l'appartement correspondant
-    // et affichez-les dans votre composant
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const nextSlide = () => {
+      setCurrentSlide(currentSlide === logement.pictures.length - 1 ? 0 : currentSlide + 1);
+    };
+
+    console.log(logement.pictures.length);
+    console.log('hello');
+
+    console.log(nextSlide);
+
+    console.log('hello2');
+
+    console.log(currentSlide);
+
+    const prevSlide = () => {
+      setCurrentSlide(currentSlide === 0 ? logement.pictures.length - 1 : currentSlide - 1);
+    };
+
+    console.log(prevSlide);
   
     return (
       <div className = "carroussel">
-        {logement.pictures.map((picture, title, index) => (<img className="images-galerie" key={id} src={picture} alt={`${title + index + 1}`} />))}       
+        <div className="slides-container">
+          {logement.pictures.map((picture, title, index) => (
+            <div
+              className={`slide ${index === currentSlide ? 'active' : ''}`}
+              key={`${index}-${title}-${id}+"b"`}
+              style={{
+                backgroundImage: `url(${picture})`,
+              }}
+            ></div>
+          ))}
+        </div>
+        <button className="prev-button" onClick={prevSlide}>
+        Previous
+        </button>
+        <button className="next-button" onClick={nextSlide}>
+        Next
+        </button>
       </div>
     );
   }
