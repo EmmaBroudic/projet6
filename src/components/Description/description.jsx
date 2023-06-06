9934726faa0cc64cb05b0d3b1ba00deb61619269
EmmaBroudic <emma.broudic35@gmail.com>
2023-06-06 12:43:45 +0200

    modif bouton decription

--- src/components/Description/description.jsx
+++ src/components/Description/description.jsx
@@ -1,7 +1,8 @@
 import { annonces } from '../../data/annonces/annonces.jsx';
 import { useParams } from 'react-router-dom';
 import React, { useState } from 'react';
-
+import flechhaut from '../../images/fleche-haut.png';
+import flechbas from '../../images/fleche-bas.png';
 import './description.css';
 import '../../index.css';
 
@@ -10,6 +11,9 @@ function Description() {
     const { id } = useParams(); // Récupère l'identifiant de l'appartement depuis l'URL
 
     const logement = annonces.find(appartement => appartement.id === id);
+
+    console.log(logement);
+    console.log("Poppie");
 
     const { description } = logement;
 
@@ -23,9 +27,15 @@ function Description() {
     <div className="bloc-description">
         <div className="bloc-titre">
             <h2 className="titre-logement">Description</h2>
-            <button className="toggle-button" onClick={toggleDescription}>
-            {descriptionVisible ? 'Fermer' : 'Ouvrir'}
-            </button>
+            {descriptionVisible ? (
+              <button className = "buttonup" onClick = {() => toggleDescription(descriptionVisible)}>
+                <img src={flechhaut} alt = "flèche vers le haut" />
+              </button>
+            ) : (
+              <button className = "buttondown" onClick={() => toggleDescription(descriptionVisible)}>
+                <img src = {flechbas} alt = "flèche vers le bas" />
+              </button>
+            )}
         </div>
         {descriptionVisible && <div className="texte-logement">{description}</div>}
     </div>
